e66b311dc53ed3f244c302b49daf89ddacb4d966
lasse <lasse@dev-lasse>
2020-04-18 12:07:32 +0200

    Bootstrap stub dev

--- tools/bootstrap.sh
+++ tools/bootstrap.sh
@@ -111,6 +111,6 @@ if [ ! -d "$SA_PATH_REPO/.git" ]; then
 	echo "Cloning serveradmin-repo..."
 	sudo -u $SA_USER git -c core.sshCommand="ssh -i $SA_DEPLOY_KEY" clone $SA_REPO "$SA_PATH/workspace/serveradmin"
 else
-    cd $SA_PATH_REPO
-    sudo -u $SA_USER git -c core.sshCommand="ssh -i $SA_DEPLOY_KEY" pull
+	cd $SA_PATH_REPO
+	sudo -u $SA_USER git -c core.sshCommand="ssh -i $SA_DEPLOY_KEY" pull
 fi
